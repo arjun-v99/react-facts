b9404e4bcf9ed97b3b1c603a35ecab1e3a21b7ce
Arjun Raj <arjunrajvilakkat@outlook.com>
2025-10-26 09:52:43 +0530

    First react project

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+export default function Main() {
+  return (
+    <main className="container">
+      <section>
+        <h1 className="main-title">Fun facts about React</h1>
+        <ul className="facts-list">
+          <li className="fact-list-item">First was released in 2013</li>
+          <li className="fact-list-item">
+            Was originally created by Jordan Walke
+          </li>
+          <li className="fact-list-item">Has well over 200k stars on Github</li>
+          <li className="fact-list-item">Is maintained by Meta</li>
+          <li className="fact-list-item">
+            Powers thousands of enterprise apps, inclding mobile apps
+          </li>
+        </ul>
+      </section>
+    </main>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <header className="header-container">
+      <nav className="navbar-nav">
+        <img
+          src="/images/reactjs-icon.png"
+          alt="react-icon"
+          className="navbar-logo"
+        />
+        <span className="navbar-title">ReactFacts</span>
+      </nav>
+    </header>
+  );
+}
